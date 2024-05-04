b66d65a4b6b4b332663f399e5c4a34c9cf1fba56
Doodle <doodlebunnyhops@gmail.com>
2024-05-04 15:49:00 -0400

    fix Farming Level, select 0

--- js/main.js
+++ js/main.js
@@ -1343,7 +1343,7 @@ function updateData() {
 	options.fertilizerSource = parseInt(document.getElementById('speed_gro_source').value);
 
 	if (document.getElementById('farming_level').value <= 0)
-		document.getElementById('farming_level').value = 1;
+		document.getElementById('farming_level').value = 0;
 	if (document.getElementById('farming_level').value > 13)
 		document.getElementById('farming_level').value = 13;
 	options.level = parseInt(document.getElementById('farming_level').value);
